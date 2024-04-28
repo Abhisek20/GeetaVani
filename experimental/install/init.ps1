9ca3bd00630158901b837a402dfc1bbfc6ed8955
abhisekg9516@gmail.com <abhisekg9516@gmail.com>
2024-04-28 21:41:06 +0530

    add experimental folder

---
+++ experimental/install/init.ps1
@@ -0,0 +1,7 @@
+mkdir data/chunks
+mkdir db
+wget https://ollama.com/download/OllamaSetup.exe
+./OllamaSetup.exe
+ollama pull llama3
+ollama pull phi3
+
---
+++ experimental/install/init.sh
@@ -0,0 +1,6 @@
+mkdir data/chunks
+mkdir db
+curl -fsSL https://ollama.com/install.sh | sh
+ollama pull llama3
+ollama pull phi3
+
